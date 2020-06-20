68e03e1aa7a59facb6c47cc8625806ec0533fd05
error_occured <shubhanshujha17@gmail.com>
2020-06-20 21:58:55 +0530

    Added tabs(LiveTest, MockTest, PreviousPaper) in dashboard

---
+++ app/src/main/java/com/example/itifighter/MyAdapter.java
@@ -0,0 +1,36 @@
+package com.example.itifighter;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+class MyAdapter extends FragmentPagerAdapter {
+    Context context;
+    int totalTabs;
+    public MyAdapter(Context c, FragmentManager fm, int totalTabs) {
+        super(fm);
+        context = c;
+        this.totalTabs = totalTabs;
+    }
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                PreviousPaper PPFragment = new PreviousPaper();
+                return PPFragment;
+            case 1:
+                MockTest MTFragment = new MockTest();
+                return MTFragment;
+            case 2:
+                LiveTest LTFragment = new LiveTest();
+                return LTFragment;
+            default:
+                return null;
+        }
+    }
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
