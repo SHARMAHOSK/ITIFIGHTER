6e7e612cad274353f6eac8207f34701251f79e8a
error_occured <shubhanshujha17@gmail.com>
2020-06-24 02:06:37 +0530

    Add ListView for branches in Previous Papers Tab.

--- app/src/main/java/com/example/itifighter/PreviousPaper.java
+++ app/src/main/java/com/example/itifighter/PreviousPaper.java
@@ -2,11 +2,26 @@ package com.example.itifighter;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +38,9 @@ public class PreviousPaper extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private View ppView;
+    private FirebaseFirestore db;
 
     public PreviousPaper() {
         // Required empty public constructor
@@ -53,12 +71,38 @@ public class PreviousPaper extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_previous_paper, container, false);
+        ppView =  inflater.inflate(R.layout.fragment_previous_paper, container, false);
+        CustomizeView(ppView);
+        return ppView;
+    }
+
+    private void CustomizeView(final View _ppView) {
+        //TextView tv = _ppView.findViewById(R.id.ppTextView);
+        db.collection("branch").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    List<String> list = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        list.add(document.getString("Name"));
+                    }
+                    Log.d(TAG, list.toString());
+                    ArrayAdapter adapter = new ArrayAdapter<String>(getContext(),
+                            R.layout.activity__branch_list_view, list);
+
+                    ListView listView = (ListView) _ppView.findViewById(R.id.branch_list);
+                    listView.setAdapter(adapter);
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
     }
 }
