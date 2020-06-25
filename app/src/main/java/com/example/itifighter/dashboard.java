71ad1a1a506cbf69f9bfef2435ab433e1c96f96e
SHARMAHOSK <sk19091997sk@gmail>
2020-06-26 01:57:24 +0530

    menu bar genrated and home fragment set as a activity_dashboard.xml

--- app/src/main/java/com/example/itifighter/dashboard.java
+++ app/src/main/java/com/example/itifighter/dashboard.java
@@ -1,19 +1,14 @@
 package com.example.itifighter;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class dashboard extends AppCompatActivity {
-    private FirebaseAuth mFirebaseAuth;
-    TabLayout tabLayout;
-    ViewPager viewPager;
+public class dashboard extends AppCompatActivity{
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +25,8 @@ public class dashboard extends AppCompatActivity {
             }
         });*/
 
-
-        tabLayout = findViewById(R.id.tabLayout);
-        viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayoutX);
+        viewPager = findViewById(R.id.viewPagerX);
         tabLayout.addTab(tabLayout.newTab().setText("Previous Paper"));
         tabLayout.addTab(tabLayout.newTab().setText("Mock Test"));
         tabLayout.addTab(tabLayout.newTab().setText("Daily Live Test"));
