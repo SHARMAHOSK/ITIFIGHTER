8dbc894ff03d63e949f34abd58300308072f44b0
SHARMAHOSK <sk19091997sk@gmail>
2020-06-27 14:43:14 +0530

    layout

--- app/src/main/java/com/example/itifighter/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/itifighter/ui/gallery/GalleryFragment.java
@@ -19,7 +19,7 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel = ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_Menu_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
--- app/src/main/java/com/example/itifighter/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/itifighter/ui/home/HomeFragment.java
@@ -19,7 +19,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_Menu_home, container, false);
 
         /*Button logout = findViewById(R.id.logout);
         mFirebaseAuth = FirebaseAuth.getInstance();
