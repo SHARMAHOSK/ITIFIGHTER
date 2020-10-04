006c18cb29efa50b998ffb261fd588524f25a2ed
Tech-pooja <pos08041999pos@gmail.com>
2020-10-04 18:23:06 +0700

    leader bord cross button updated

---
+++ app/src/main/java/com/example/itifighter/LiveTestXyz.java
@@ -0,0 +1,14 @@
+package com.example.itifighter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LiveTestXyz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_test_xyz);
+    }
+}
