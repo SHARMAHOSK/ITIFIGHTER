49b2c317feb66facd2b71a6299cf06535a9b94e2
Tech-pooja <pos08041999pos@gmail.com>
2020-10-06 02:25:35 +0700

    mock chapter new design updated

---
+++ app/src/main/java/com/example/itifighter/MockChapterXyz.java
@@ -0,0 +1,14 @@
+package com.example.itifighter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MockChapterXyz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mock_chapter_xyz);
+    }
+}
