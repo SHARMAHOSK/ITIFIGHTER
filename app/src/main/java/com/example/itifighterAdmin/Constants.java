52786fa986a7c6c364dce229926e0827fd2796a5
Tech-pooja <pos08041999pos@gmail.com>
2020-12-18 18:17:32 +0700

    delete data

--- app/src/main/java/com/example/itifighterAdmin/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.itifighterAdmin;
-
-public class Constants {
-    public static final String STORAGE_PATH_UPLOADS = "uploads/";
-    public static final String STORAGE_PATH_LOGOS = /*"logos/"*/"menu/section/";
-}
